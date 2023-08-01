4e5a0ac8793b9f9fb9eba1c3958702673164b185
Chriistopher Millsap <christopher.millsap@gmail.com>
2023-07-31 21:51:19 -0500

    Version Change

--- buildVars.py
+++ buildVars.py
@@ -26,7 +26,7 @@ addon_info = {
 	"addon_description": _("""Provides captions for selected images in Windows Explorer, Chrome, Edge, and FireFox. Uses the BLIP neural
 	 network to generate these captions """),
 	# version
-	"addon_version": "0.8.1",
+	"addon_version": "0.8.2",
 	# Author(s)
 	"addon_author": "Christopher Millsap <christopher.millsap@outlook.com>",
 	# URL for the add-on documentation support

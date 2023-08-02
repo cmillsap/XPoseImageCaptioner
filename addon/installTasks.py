82e178ac10a8c1f5bba8ba5baa030f4a6fd09a9a
Chriistopher Millsap <christopher.millsap@gmail.com>
2023-08-01 20:57:35 -0500

    update to provide failure message if nn download not possivle

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -19,9 +19,4 @@ def copyURLtoFile(url, tempFilename):
 def onInstall():
 	#build a path to the addon directory and weights path 
 	weightsPath = os.path.join(globalVars.appArgs.configPath, 'addons\\\XPoseImage Captioner.pendingInstall\\globalPlugins\\dist\\weights\\model_base_caption_capfilt_large.pth')
-	try:
-		copyURLtoFile('https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_caption_capfilt_large.pth',weightsPath)
-	except Exception as e: 
-		log.error(f"Exception = {e}")
-		traceback.print_exception(type(e), e, e.__traceback__)
-		ui.message("BLIP Neural Network wieghts could not be downloaded. Installation can not proceed. ")
+	copyURLtoFile('https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_caption_capfilt_large.pth',weightsPath)
